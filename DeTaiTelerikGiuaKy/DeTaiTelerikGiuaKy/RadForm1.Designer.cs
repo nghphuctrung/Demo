7c9fc3684e552181fc910470e05435877aa7473f
nghphuctrung <ng.h.phuctrung@gmail.com>
2016-08-20 16:35:31 +0700

    Them file visual

---
+++ DeTaiTelerikGiuaKy/DeTaiTelerikGiuaKy/RadForm1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace DeTaiTelerikGiuaKy
+{
+    partial class RadForm1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.radLabel1 = new Telerik.WinControls.UI.RadLabel();
+            this.radLabel2 = new Telerik.WinControls.UI.RadLabel();
+            this.lbconnect = new Telerik.WinControls.UI.RadLabel();
+            this.btnLogin = new Telerik.WinControls.UI.RadButton();
+            this.txtAccounnt = new Telerik.WinControls.UI.RadTextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.txtPassword = new Telerik.WinControls.UI.RadTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lbconnect)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnLogin)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtAccounnt)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtPassword)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // radLabel1
+            // 
+            this.radLabel1.Location = new System.Drawing.Point(55, 39);
+            this.radLabel1.Name = "radLabel1";
+            this.radLabel1.Size = new System.Drawing.Size(47, 18);
+            this.radLabel1.TabIndex = 0;
+            this.radLabel1.Text = "Account";
+            // 
+            // radLabel2
+            // 
+            this.radLabel2.Location = new System.Drawing.Point(55, 77);
+            this.radLabel2.Name = "radLabel2";
+            this.radLabel2.Size = new System.Drawing.Size(53, 18);
+            this.radLabel2.TabIndex = 0;
+            this.radLabel2.Text = "Password";
+            // 
+            // lbconnect
+            // 
+            this.lbconnect.ForeColor = System.Drawing.Color.Red;
+            this.lbconnect.Location = new System.Drawing.Point(12, 125);
+            this.lbconnect.Name = "lbconnect";
+            this.lbconnect.Size = new System.Drawing.Size(2, 2);
+            this.lbconnect.TabIndex = 0;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(55, 166);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(110, 24);
+            this.btnLogin.TabIndex = 1;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // txtAccounnt
+            // 
+            this.txtAccounnt.Location = new System.Drawing.Point(134, 39);
+            this.txtAccounnt.Name = "txtAccounnt";
+            this.txtAccounnt.Size = new System.Drawing.Size(100, 20);
+            this.txtAccounnt.TabIndex = 2;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(55, 109);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(107, 17);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Show Password";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(134, 75);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '●';
+            this.txtPassword.Size = new System.Drawing.Size(100, 20);
+            this.txtPassword.TabIndex = 2;
+            this.txtPassword.UseSystemPasswordChar = true;
+            // 
+            // RadForm1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 261);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtAccounnt);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.lbconnect);
+            this.Controls.Add(this.radLabel2);
+            this.Controls.Add(this.radLabel1);
+            this.Name = "RadForm1";
+            // 
+            // 
+            // 
+            this.RootElement.ApplyShapeToControl = true;
+            this.Text = "RadForm1";
+            this.Load += new System.EventHandler(this.RadForm1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lbconnect)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnLogin)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtAccounnt)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtPassword)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Telerik.WinControls.UI.RadLabel radLabel1;
+        private Telerik.WinControls.UI.RadLabel radLabel2;
+        private Telerik.WinControls.UI.RadLabel lbconnect;
+        private Telerik.WinControls.UI.RadButton btnLogin;
+        private Telerik.WinControls.UI.RadTextBox txtAccounnt;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private Telerik.WinControls.UI.RadTextBox txtPassword;
+    }
+}
---
+++ DeTaiTelerikGiuaKy/DeTaiTelerikGiuaKy/RadForm1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using Telerik.WinControls.UI;
+using Telerik.WinControls.CodedUI;
+
+
+namespace DeTaiTelerikGiuaKy
+{
+    public partial class RadForm1 : Telerik.WinControls.UI.RadForm
+    {
+        SqlConnection cn;
+        string cnStr = @"Server = .; Database = Login ; Integrated security = true";
+        SqlCommand cmd;
+
+        public RadForm1()
+        {
+            InitializeComponent();
+        }
+
+        private void RadForm1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                cn = new SqlConnection(cnStr);
+                cn.Open();
+                string sql = "Select count(*) From [Login].[dbo].[TKLogin] Where TaiKhoan = @acc And MatKhau = @pass ";
+                cmd = new SqlCommand(sql, cn);
+                cmd.Parameters.Add(new SqlParameter("@acc", txtAccounnt.Text));
+                cmd.Parameters.Add(new SqlParameter("@pass", txtPassword.Text));
+                int x = (int)cmd.ExecuteScalar();
+                if (x == 1)
+                {
+                    MessageBox.Show("Đăng Nhập Thành Công !", "Nofitication");
+                    //this.Hide();
+                    //fm = new Form2();
+                    //fm.Show();
+                }
+                else
+                {
+                    lbconnect.Text = "Tài Khoản hoặc mật khẩu không thể đăng nhập";
+                    txtAccounnt.Text = "";
+                    txtPassword.Text = "";
+                    txtAccounnt.Focus();
+                    txtPassword.Focus();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+      
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                txtPassword.UseSystemPasswordChar = false;
+            }
+            else
+            {
+                txtPassword.UseSystemPasswordChar = true;
+            }
+        }
+    }
+}
